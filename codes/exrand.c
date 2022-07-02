4a21860f76e2d6773e9faee18893220c193569d0
JBA-12 <102513876+JBA-12@users.noreply.github.com>
2022-07-02 16:22:48 +0530

    Add files via upload

---
+++ codes/exrand.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "coeffs.h"
+
+int  main(void) //main function begins
+{
+ 
+//Part - 1
+//Uniform random numbers
+uniform("uni.dat", 1000000);
+
+//Part-2
+//Gaussian random numbers
+gaussian("gau.dat", 1000000);
+
+return 0;
+}
+
