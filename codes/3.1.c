bfd2182b5fb1f9cb8052e332448805dc598de216
JBA-12 <102513876+JBA-12@users.noreply.github.com>
2022-07-04 20:12:12 +0530

    Create 3.1.c

---
+++ codes/3.1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "coeffs.h"
+
+int  main(void) //main function begins
+{
+  
+//random numbers of form 'V'
+v("V.dat", 1000000);
+
+return 0;
+  
+}
