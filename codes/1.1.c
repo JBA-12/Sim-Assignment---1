2987c6b06fcb9bf369d1e8b0725b24911da64aa6
JBA-12 <102513876+JBA-12@users.noreply.github.com>
2022-07-04 20:09:30 +0530

    Create 1.1.c

---
+++ codes/1.1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "coeffs.h"
+
+int  main(void) //main function begins
+{
+ 
+//Part - 1
+//Uniform random numbers
+uniform("uni.dat", 1000000);
+  
+  return 0;
+  
+}
